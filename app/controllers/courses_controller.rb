ce525df232d8231567c1fb9e69f1b969bddd6497
sasha370 <kng.sasha@mail.ru>
2020-08-10 17:04:57 +0300

    add Seach Function

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,28 +1,30 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
-  # GET /courses
-  # GET /courses.json
+# В Экшене прописываем дополнительно поиск
   def index
-    @courses = Course.all
+    # Если в параметрах передан заголовок, то ищем все курсыБ содержащие его
+    if params[:title]
+      # Запрос прописыывается вручную т.к. нужно частичное совпадение
+      @courses = Course.where("title LIKE ?", "%#{params[:title]}%") # нечувствительно к заглавным
+    else
+      # Если параметров нет, то выводим полный список курсов
+      @courses = Course.all
+    end
   end
 
-  # GET /courses/1
-  # GET /courses/1.json
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
+
   def edit
   end
 
-  # POST /courses
-  # POST /courses.json
+
   def create
     @course = Course.new(course_params)
     # У каждого курса должен быть User? поэтому берем текущего (зарегистрированного)
@@ -38,38 +40,33 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1
-  # PATCH/PUT /courses/1.json
+
   def update
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-        format.json { render :show, status: :ok, location: @course }
       else
         format.html { render :edit }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /courses/1
-  # DELETE /courses/1.json
+
   def destroy
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(:title, :description)
-    end
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
 end
