9a399650f0feec5b856539ec2a61ea2421bfbbf0
sasha370 <kng.sasha@mail.ru>
2020-08-13 14:04:58 +0300

    add Edit func for User`s role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def index
     # Выбираем всех узеров и сортируем по дате создания
@@ -8,4 +9,31 @@ class UsersController < ApplicationController
     @users = @q.result(distinct: true)
   end
 
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User #{@user.email} was successfully update."
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({ role_ids: [] })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # все пути для Курсов проаиснные генератором ысфаащдв
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
 
   root 'home#index'
 
