d16b9f80a5eca67555fff2b31d0664f80f7eb064
sasha370 <kng.sasha@mail.ru>
2020-08-20 17:27:22 +0300

    remove Logic from Contoller to Model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,19 @@ class HomeController < ApplicationController
   def index
     # отображаем список из 3х курсов
     @courses = Course.all.limit(3)
+<<<<<<< Updated upstream
+=======
+    # Все Подписки, у которых есть отзывы, 3 последнихб сортированы порейтингу
+    @latest_good_reviews = Enrollment.reviewed.latest_good_reviews
+    # Список самых свежих курсов (логику перенесли в Модель Courses)
+    @latest = Course.latest
+    # Список самых Лучших курсов
+    @top_rated = Course.top_rated
+    # Список Самых популярных курсов
+    @popular = Course.popular
+    # Мои курсы, Продолжить обучение
+    @purchased_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order( created_at: :desc).limit(3)
+>>>>>>> Stashed changes
 
     # Список самых свежих курсов
     @latest_courses = Course.all.limit(3).order(created_at: :desc)
