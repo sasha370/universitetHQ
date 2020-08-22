f1c090ea3914b31845eaec2ff322f5ca40bd20d6
sasha370 <kng.sasha@mail.ru>
2020-08-22 18:08:13 +0300

    add icon in Activity list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,25 @@
 module ApplicationHelper
   # фронтенд для пагинации
   include Pagy::Frontend
+
+  # Метод, который получает на входе имя по CRUD и в зависимости от него возыращает иконку ( используется в Activity )
+  def crud_label(key)
+    case key
+      when 'create'
+        "<i class= 'fa fa-plus'></i>".html_safe
+      when 'update'
+        "<i class= 'fa fa-pen'></i>".html_safe
+      when 'destroy'
+        "<i class= 'fa fa-trash'></i>".html_safe
+    end
+  end
+
+  def model_label(model)
+    case model
+      when 'Course'
+        "<i class= 'fa fa-graduation-cap'></i>".html_safe
+      when 'Lesson'
+        "<i class= 'fa fa-tasks'></i>".html_safe
+    end
+  end
 end
